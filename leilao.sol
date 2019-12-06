e7af82e89182b6837ce1a95c874dad55071413bc
Victor Gomes <victormenezez15@gmail.com>
2019-12-06 17:18:07 -0300

    Updates in leilao.sol

--- leilao.sol
+++ leilao.sol
@@ -9,7 +9,7 @@ contract Leilao {
     event LanceAdicionado(address bidder, uint num_item, uint valor_lance);
     event LeilaoFinalizado(uint num_item, address vencedor);
     event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
-    event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId); 
+    event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
 
     struct Item {
         address dono_lance_atual; // Endereco que esta vencendo o leilao
@@ -17,7 +17,7 @@ contract Leilao {
 
         string titulo; // Titulo do item
         string descricao; // Descricao do item
-        
+
         uint token; // Token do item (contagem de itens)
         uint valor_inicial; // Valor inicial (base) do item
         uint lance_atual; // Valor atual do item
@@ -25,8 +25,8 @@ contract Leilao {
         uint data_expiracao; // Duração do item no leilão
     }
 
-    modifier onlyOwnerOfToken (uint256 _tokenId) {
-        require(msg.sender == _tokenOwner[_tokenId]);
+    modifier onlyOwnerOfToken (address _from, uint256 _tokenId) {
+        require(_from == _tokenOwner[_tokenId]);
         _;
     }
     modifier onlyOwnerOfItem(uint256 _item) {
@@ -38,13 +38,13 @@ contract Leilao {
     mapping (address => uint256) public ownerTokenCount; // Armazena o saldo de cada conta que possui tokens
     mapping (uint256 => address) internal _tokenOwner; // Relaciona o token com o dono
     mapping (uint256 => address) tokenApprovals;
-    
+
     Item[] public itens_disponiveis;
 
     function adicionar_item(string _titulo, string _descricao, uint _valor_inicial, uint _diferenca_minima, uint _qtd_dias) public {
         count_item++;
         count_itens_disponiveis++;
-        
+
         Item memory novo_item = Item(0,
                                     msg.sender,
                                     _titulo,
@@ -58,36 +58,36 @@ contract Leilao {
         ownerTokenCount[this]++;
         _tokenOwner[novo_item.token] = this;
         itens[count_item] = novo_item;
-        
+
         itens_disponiveis.push(novo_item);
-        
+
         emit ItemAdicionado(novo_item.titulo, novo_item.descricao, novo_item.valor_inicial, novo_item.diferenca_minima, novo_item.data_expiracao);
     }
-    
+
     function balanceOf(address _owner) public view returns (uint256 _balance) {
         return ownerTokenCount[_owner];
     }
-    
+
     function _ownerOf(uint256 _tokenId) private view returns (address _owner) {
         return _tokenOwner[_tokenId];
     }
-    
+
     function transfer(address _to, uint256 _tokenId) public {
         _transfer(msg.sender, _to, _tokenId);
     }
-    
-    function _transfer(address _from, address _to, uint256 _tokenId) private onlyOwnerOfToken(_tokenId) {
+
+    function _transfer(address _from, address _to, uint256 _tokenId) private onlyOwnerOfToken(_from, _tokenId) {
         ownerTokenCount[_to]++;
         ownerTokenCount[_from]--;
         _tokenOwner[_tokenId] = _to;
         emit Transfer(_from, _to, _tokenId);
     }
-    
-    function approve(address _to, uint256 _tokenId) public onlyOwnerOfToken(_tokenId) {
+
+    function approve(address _to, uint256 _tokenId) public onlyOwnerOfToken(msg.sender, _tokenId) {
         tokenApprovals[_tokenId] = _to;
         emit Approval(msg.sender, _to, _tokenId);
     }
-    
+
     function takeOwnership(uint256 _tokenId) public {
         require(tokenApprovals[_tokenId] == msg.sender);
         address owner = _ownerOf(_tokenId);
@@ -96,7 +96,7 @@ contract Leilao {
 
     function dar_lance(uint _num_item) public payable {
         require(itens[_num_item].dono != 0, "Esse item não está disponível.");
-        
+
         Item storage item_atual = itens[_num_item];
 
         require(now < (item_atual.data_expiracao), "Este item já expirou. Verique o ganhador.");
@@ -110,7 +110,7 @@ contract Leilao {
 
         item_atual.dono_lance_atual = msg.sender;
         item_atual.lance_atual = msg.value;
-        
+
         emit LanceAdicionado(item_atual.dono_lance_atual, _num_item, item_atual.lance_atual);
     }
 
@@ -118,36 +118,36 @@ contract Leilao {
         require(itens[_num_item].dono != 0, "Esse item não está mais disponível.");
 
         Item storage item_atual = itens[_num_item];
-        
+
         if (now >= item_atual.data_expiracao) {
             _realizar_tramites(_num_item);
         } else if (item_atual.valor_inicial == item_atual.lance_atual) {
             return item_atual.dono;
         }
-        
+
         return item_atual.dono_lance_atual;
     }
 
     function finalizar_leilao(uint256 _num_item) public onlyOwnerOfItem(_num_item) {
         Item storage item_atual = itens[_num_item];
-        
+
         if (now <= (item_atual.data_expiracao)) {
             item_atual.data_expiracao = now;
         }
-        
+
         _realizar_tramites(_num_item);
-        
+
         emit LeilaoFinalizado(_num_item, item_atual.dono_lance_atual);
     }
-    
+
     function _realizar_tramites(uint256 _num_item) private {
         Item storage item_atual = itens[_num_item];
-        
+
         if (item_atual.dono_lance_atual != 0) {
             _transferir(item_atual.dono, item_atual.lance_atual);
             transfer(item_atual.dono_lance_atual, item_atual.token);
         }
-        
+
         delete itens[_num_item];
         for (uint i = 0; i < count_itens_disponiveis; i++) {
             if (itens_disponiveis[i].token == _num_item) {
@@ -155,7 +155,7 @@ contract Leilao {
             }
         }
         count_itens_disponiveis--;
-        
+
     }
 
     function _transferir(address _dono_lance_anterior, uint _valor) private {
