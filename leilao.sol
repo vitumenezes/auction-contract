6095706a2a9fad0d8536b872685994bbfa7d2d62
Victor Gomes <victormenezez15@gmail.com>
2019-12-10 12:26:43 -0300

    Adding comments

--- leilao.sol
+++ leilao.sol
@@ -2,14 +2,38 @@ pragma solidity 0.4.25;
 
 contract Leilao {
 
+    // total number of itens already added
     uint count_item;
+
+    // available items count
     uint count_itens_disponiveis;
 
+    /*
+    * @dev Emitted when a new item is added
+    **/
     event ItemAdicionado(string titulo, string descricao, uint valor_inicial, uint diferenca_minima, uint data_expiracao);
+
+    /*
+    * @dev Emitted when a new bid is added to a item
+    **/
     event LanceAdicionado(address bidder, uint num_item, uint valor_lance);
+
+    /*
+    * @dev Emitted when an auction is finished
+    **/
     event LeilaoFinalizado(uint num_item, address vencedor);
+
+    /*
+    * @dev Emitted when a token transfer is made
+    **/
     event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
+
+    /*
+    * @dev Emitted when a token transfer approval is made
+    **/
     event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
+
+
 
     struct Item {
         address dono_lance_atual; // Endereco que esta vencendo o leilao
@@ -25,22 +49,49 @@ contract Leilao {
         uint data_expiracao; // Duração do item no leilão
     }
 
+    /*
+    * @dev Modifier to make a function callable only when the sender is the token owner
+    **/
     modifier onlyOwnerOfToken (address _from, uint256 _tokenId) {
         require(_from == _tokenOwner[_tokenId]);
         _;
     }
+
+    /*
+    * @dev Modifier to make a function callable only when the sender is the item owner
+    **/
     modifier onlyOwnerOfItem(uint256 _item) {
         require(msg.sender == itens[_item].dono, "Você não pode finalizar um leilao do qual não é o dono.");
         _;
     }
 
-    mapping (uint => Item) public itens; // mapping de itens
-    mapping (address => uint256) public ownerTokenCount; // Armazena o saldo de cada conta que possui tokens
-    mapping (uint256 => address) internal _tokenOwner; // Relaciona o token com o dono
+    // Items mapping. Each item has one uint to represent it
+    mapping (uint => Item) public itens;
+
+    // Token balance on an account - ERC721
+    mapping (address => uint256) public ownerTokenCount;
+
+    // Relates the token with the owner - ERC721
+    mapping (uint256 => address) internal _tokenOwner;
+
+    // Token transfer approvals - ERC721
     mapping (uint256 => address) tokenApprovals;
 
+    // Available itens to auction
     Item[] public itens_disponiveis;
 
+
+    /*
+    * @dev Public function to add a new item to auctioned.
+    *
+    * Emits an {ItemAdicionado} event.
+    *
+    * @param _titulo item title.
+    * @param _descricao item description.
+    * @param _valor_inicial initial value of item.
+    * @param _diferenca_minima minimum difference between bids.
+    * @param _qtd_dias number of days the item will be up for auction.
+    **/
     function adicionar_item(string _titulo, string _descricao, uint _valor_inicial, uint _diferenca_minima, uint _qtd_dias) public {
         count_item++;
         count_itens_disponiveis++;
@@ -64,18 +115,31 @@ contract Leilao {
         emit ItemAdicionado(novo_item.titulo, novo_item.descricao, novo_item.valor_inicial, novo_item.diferenca_minima, novo_item.data_expiracao);
     }
 
+
+    /*
+    * @dev Implementation of {ERC721} interface
+    **/
     function balanceOf(address _owner) public view returns (uint256 _balance) {
         return ownerTokenCount[_owner];
     }
 
+    /*
+    * @dev Implementation of {ERC721} interface
+    **/
     function _ownerOf(uint256 _tokenId) private view returns (address _owner) {
         return _tokenOwner[_tokenId];
     }
 
+    /*
+    * @dev Implementation of {ERC721} interface
+    **/
     function transfer(address _to, uint256 _tokenId) public {
         _transfer(this, _to, _tokenId);
     }
 
+    /*
+    * @dev Implementation of {ERC721} interface
+    **/
     function _transfer(address _from, address _to, uint256 _tokenId) private onlyOwnerOfToken(_from, _tokenId) {
         ownerTokenCount[_to]++;
         ownerTokenCount[_from]--;
@@ -83,11 +147,17 @@ contract Leilao {
         emit Transfer(_from, _to, _tokenId);
     }
 
+    /*
+    * @dev Implementation of {ERC721} interface
+    **/
     function approve(address _to, uint256 _tokenId) public onlyOwnerOfToken(msg.sender, _tokenId) {
         tokenApprovals[_tokenId] = _to;
         emit Approval(msg.sender, _to, _tokenId);
     }
 
+    /*
+    * @dev Implementation of {ERC721} interface
+    **/
     function takeOwnership(uint256 _tokenId) public {
         require(tokenApprovals[_tokenId] == msg.sender);
         address owner = _ownerOf(_tokenId);
